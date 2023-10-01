52e592ab8810837d22cb07a6d107e557f24afdcc
ykhdr <yanhudorozhkov@gmail.com>
2023-10-01 18:53:14 +0700

    Added sql requests to first lab

---
+++ scientific-periodicals/functions.sql
@@ -0,0 +1,17 @@
+CREATE SEQUENCE publisher_issue_sequence;
+
+CREATE OR REPLACE FUNCTION insert_publisher_publication(
+    publisher_id bigint,
+    publication_id bigint,
+    publication_date date
+)
+    RETURNS VOID AS
+$$
+BEGIN
+    INSERT INTO Publishers_Publications (publisher_id, publication_id, issue, publication_date)
+    VALUES (publisher_id,
+            publication_id,
+            nextval('publisher_issue_sequence'),
+            publication_date);
+END;
+$$ LANGUAGE plpgsql;
