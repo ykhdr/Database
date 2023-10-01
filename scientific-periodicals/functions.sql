5ad143dbc8cc7b6f22d29766ec26b6cb8e502f01
ykhdr <yanhudorozhkov@gmail.com>
2023-10-01 20:49:18 +0700

    Commented function

--- scientific-periodicals/functions.sql
+++ scientific-periodicals/functions.sql
@@ -1,17 +1,24 @@
-CREATE SEQUENCE publisher_issue_sequence;
 
-CREATE OR REPLACE FUNCTION insert_publisher_publication(
-    publisher_id bigint,
-    publication_id bigint,
-    publication_date date
-)
-    RETURNS VOID AS
-$$
-BEGIN
-    INSERT INTO Publishers_Publications (publisher_id, publication_id, issue, publication_date)
-    VALUES (publisher_id,
-            publication_id,
-            nextval('publisher_issue_sequence'),
-            publication_date);
-END;
-$$ LANGUAGE plpgsql;
+-- Написать функцию добавления издательства,
+-- чтобы в ней создавался новый сиквенс под это издательство?
+
+
+-- CREATE SEQUENCE publisher_issue_sequence;
+--
+--
+--
+-- CREATE OR REPLACE FUNCTION insert_publisher_publication(
+--     publisher_id bigint,
+--     publication_id bigint,
+--     publication_date date
+-- )
+--     RETURNS VOID AS
+-- $$
+-- BEGIN
+--     INSERT INTO Publishers_Publications (publisher_id, publication_id, issue, publication_date)
+--     VALUES (publisher_id,
+--             publication_id,
+--             nextval('publisher_issue_sequence'),
+--             publication_date);
+-- END;
+-- $$ LANGUAGE plpgsql;
